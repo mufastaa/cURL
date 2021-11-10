efdfea968a4f9639c18de2c37e61f2ea7555396d
mufastaa <92852873+mufastaa@users.noreply.github.com>
2021-11-09 23:11:33 -0800

    Update create.php

--- create.php
+++ create.php
@@ -1,6 +1,6 @@
 <?php
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://localhost/php-crash/14_product_crud/01_bad/index.php');
+curl_setopt($ch, CURLOPT_URL, 'http://localhost/simple-project-/');
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
